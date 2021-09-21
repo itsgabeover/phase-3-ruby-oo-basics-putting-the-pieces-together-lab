f3c743c0f0bfe4387942d2b89428fa836e4be053
Gabriel Miranda <gabe.enrique.miranda@gmail.com>
2021-09-20 20:07:28 -0700

    completed

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,20 @@
+ # Book .new gets initialized with a title
+# Book properties has a title
+ # Book properties has an author name
+ # Book properties has a page count
+# Book properties has a genre
+ # Book #turn_page can turn the page
+ require 'pry'
 
-
+ class Book
+    attr_accessor :author, :page_count, :genre
+    attr_reader :title
+  
+    def initialize(title)
+      @title = title
+    end
+  
+    def turn_page
+      puts "Flipping the page...wow, you read fast!"
+    end
+  end
